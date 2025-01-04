755da652a135e9a6a9f053efbfa143597e85f39d
Jon Robison <narfman0@gmail.com>
2025-01-03 23:55:20 -0500

    add fps

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include "flecs.h"
 
+float FPS = 6.0f;
+
 struct Position
 {
     float x, y;
@@ -14,9 +16,10 @@ struct Velocity
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Initializing flecs!\n";
+    std::cout << "Initializing flecs\n";
     flecs::world ecs;
     ecs.set<flecs::Rest>({});
+    ecs_set_target_fps(ecs, FPS);
 
     ecs.system<Position, const Velocity>()
         .each([](Position &p, const Velocity &v)
@@ -24,14 +27,15 @@ int main(int argc, char *argv[])
       p.x += v.x;
       p.y += v.y; });
 
-    auto e = ecs.entity()
-                 .insert([](Position &p, Velocity &v)
-                         {
+    ecs.entity()
+        .insert([](Position &p, Velocity &v)
+                {
       p = {10, 20};
       v = {1, 2}; });
 
     while (ecs.progress())
     {
+        std::cout << "FLECS progressed\n";
     }
-    std::cout << "Done with flecs!\n";
+    std::cout << "Exiting\n";
 }
