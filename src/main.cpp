20033fab0b956616363c0cd4cabf63fd5ea16f1a
Jon Robison <narfman0@gmail.com>
2025-01-03 23:21:11 -0500

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "flecs.h"
+
+struct Position
+{
+    float x, y;
+};
+
+struct Velocity
+{
+    float x, y;
+};
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Initializing flecs!\n";
+    flecs::world ecs;
+
+    ecs.system<Position, const Velocity>()
+        .each([](Position &p, const Velocity &v)
+              {
+      p.x += v.x;
+      p.y += v.y; });
+
+    auto e = ecs.entity()
+                 .insert([](Position &p, Velocity &v)
+                         {
+      p = {10, 20};
+      v = {1, 2}; });
+
+    while (ecs.progress())
+    {
+    }
+    std::cout << "Done with flecs!\n";
+}
